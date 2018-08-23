58a7e5da6eb073c0776d4022323129f554962974
Lionel Pedro <lionel.pedro@otto.de>
2018-08-23 13:13:25 +0200

    ufff... trying to get this going

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ cd ~ && mkdir -p .vim/{bundle,autoload,view}
 cd ~ && mkdir Vimpossible
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cd ~/Vimpossible && git clone https://github.com/powerline/powerline.git && git clone https://github.com/NLKNguyen/papercolor-16.git
-cat ~/Vimpossible/.bashrc > ~/.bashrc
-cat ~/Vimpossible/.vimrc > ~/.vimrc
+cat ~/Vimpossible/.bashrc >> ~/.bashrc
+cat ~/Vimpossible/.vimrc >> ~/.vimrc
 cd ~/Vimpossible/NLKNguyen/papercolor-16/gnome-terminal && ./base16-papercolor-dark.sh
 cd ~/Vimpossible && cp PaperColor.vim ~/.vim/bundle//vim-airline-themes/autoload/airline/themes/
 cd ~/Vimpossible && cp papercolor.vim ~/.vim/bundle//vim-airline-themes/autoload/airline/themes/
